42f375581bed8a382527fbe8d5c59aa0dbc6f9da
DenWebSite <evgenjuk12@gmail.com>
2025-05-07 21:22:09 +0300

    create rate list for case.html (and case.js) and fix .contacts__list-link's svg icon

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 const btn = document.querySelector('.menu-btn');
 const header__nav = document.querySelector('.header__nav');
 
-btn.addEventListener('click', ()=>{
+btn.addEventListener('click', () => {
     header__nav.classList.toggle('menu-open');
 });
 
 const tg = document.querySelector('.tg');
 const tg__show = document.querySelector('.tg-show');
 
-tg.addEventListener('click', ()=>{
+tg.addEventListener('click', () => {
     tg__show.classList.toggle('visually-hidden');
 });
