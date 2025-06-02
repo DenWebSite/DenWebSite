44574511211f66b9506a16ca509f0a2e229a96c1
DenWebSite <evgenjuk12@gmail.com>
2025-06-02 20:46:06 +0300

    update html, fix quotes-images and create anchor link

--- js/script.js
+++ js/script.js
@@ -4,10 +4,3 @@ const header__nav = document.querySelector('.header__nav');
 btn.addEventListener('click', () => {
     header__nav.classList.toggle('menu-open');
 });
-
-const tg = document.querySelector('.tg');
-const tg__show = document.querySelector('.tg-show');
-
-tg.addEventListener('click', () => {
-    tg__show.classList.toggle('visually-hidden');
-});
